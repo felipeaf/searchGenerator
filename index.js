475024a9b4f7b9baa670476da03aafc68d43b1a7
Felipe Ferreira <fferreira@casan.com.br>
2023-07-04 13:59:54 -0300

    bugfix

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import ArrayRandomIterator from "./ArrayRandomIterator.js";
 import lista from "./br-utf8.js";
-import BuscaIt from "./buscaIt.js";
+import SearchIt from "./buscaIt.js";
 import GeraBuscaView from "./geraBuscaView.js";
-import configParser from "./uRLParamConfigParser.js"
+import ConfigParser from "./uRLParamConfigParser.js"
 
 
 function main() {
-    const configParser = new configParser();
+    const configParser = new ConfigParser();
     const config = configParser.config;
     if(!config) return;
     const listaIt = new ArrayRandomIterator(lista,config.max)
-    const buscaIt = new BuscaIt(config.url, listaIt);
+    const buscaIt = new SearchIt(config.url, listaIt);
     const geraBuscaView = new GeraBuscaView(buscaIt);
     geraBuscaView.run();
 }
