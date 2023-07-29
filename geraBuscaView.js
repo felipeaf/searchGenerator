fcdd3993d8579a0da17d2ee381274dc3c46fae6e
Felipe Ferreira <fferreira@casan.com.br>
2023-07-29 00:52:13 -0300

    stop button

--- geraBuscaView.js
+++ geraBuscaView.js
@@ -1,8 +1,15 @@
 import { getRandomArbitrary } from "./utils.js"
 
+const STATES = {
+    "RUNNING": Symbol("RUNNING"),
+    "STOPPED": Symbol("STOPPED")
+}
+
 export default class GeraBuscaView {
     constructor(_buscaIt) {
         this._buscaIt = _buscaIt;
+        this._state = STATES.STOPPED;
+        document.getElementById("stopButton").addEventListener("click", this.stop.bind(this));
     }
 
     _randomTimeout() {
@@ -10,10 +17,24 @@ export default class GeraBuscaView {
     }
 
     run() {
+        this._state= STATES.RUNNING;
+        this._runLoop();
+    }
+
+    _runLoop() {
         const atual = this._buscaIt.next();
-        if(atual.done) {return};
+        
+        if(atual.done) return;
+        
+        if(this._state !== STATES.RUNNING) return;
+        
         window.open(atual.value, '_blank');
         //console.log(`${new Date()} ${atual.value}`)
-        setTimeout(() => this.run(), this._randomTimeout());
+        setTimeout(() => this._runLoop(), this._randomTimeout());
+    }
+
+    stop() {
+        console.log("stop clicked", this._state)
+        this._state = STATES.STOPPED;
     }
 }
